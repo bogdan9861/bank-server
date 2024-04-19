ae4b9a44dd7a73186e596e6d995c140173f4b39a
bogdan0950 <besedin_b@bk.ru>
2024-04-19 10:48:29 +0300

    cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors({origin: "https://wonderful-cat-6c2c99.netlify.app"}));
+app.use(cors({origin: "*"}));
 app.use(logger('dev'));
 
 app.use(express.json());
