e543396c0b99cd501001aaecfdad5eb84781e1f7
bogdan9861 <besedin_b@bk.ru>
2024-04-25 18:38:24 +0300

    change cors origin

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors({origin: "*"}));
-app.use(logger('dev'));
+app.use(cors({ origin: "https://wonderful-cat-6c2c99.netlify.app" }));
+app.use(logger("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
